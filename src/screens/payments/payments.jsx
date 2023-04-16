d4c9538f1d4183a5582a4646a3afd81b15800fdc
Upeksha Fernando <meenufernando18@gmail.com>
2023-04-17 01:47:30 +0530

    Update payments.jsx

--- src/screens/payments/payments.jsx
+++ src/screens/payments/payments.jsx
@@ -1,7 +1,9 @@
-import { Box, useTheme, Button } from "@mui/material";
+import { Box, useTheme, IconButton, Grid, Typography } from "@mui/material";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import { useNavigate } from "react-router-dom";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Payment = () => {
   const theme = useTheme();
@@ -19,21 +21,23 @@ const Payment = () => {
         <Header title="PAYMENT" subtitle="Welcome to Payments Page" />
       </Box>
 
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="center"
-        margin={"5%"}
-      >
-        <Button
-          type="submit"
-          color="secondary"
-          variant="contained"
-          onClick={handleButtonClick}
+      <Typography marginLeft="3%">Search for Student ID to proceed</Typography>
+
+      {/* SEARCH BAR and ICON BUTTON*/}
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Box
+          display="flex"
+          backgroundColor={colors.primary[400]}
+          borderRadius="3px"
+          margin="2%"
+          marginLeft="5%"
         >
-          Search Student
-        </Button>
-      </Box>
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+      </Grid>
     </Box>
   );
 };
