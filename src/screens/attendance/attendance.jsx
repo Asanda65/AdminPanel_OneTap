f9b29b63b6ac2051e835dfcb7982855e54a9a56e
Upeksha Fernando <meenufernando18@gmail.com>
2023-04-17 01:52:58 +0530

    Update attendance.jsx

--- src/screens/attendance/attendance.jsx
+++ src/screens/attendance/attendance.jsx
@@ -1,16 +1,12 @@
-import { Box, useTheme, Button } from "@mui/material";
+import { Box, useTheme, IconButton, Grid, Typography } from "@mui/material";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
-import { useNavigate } from "react-router-dom";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Attendance = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const navigate = useNavigate();
-
-  const handleButtonClick = () => {
-    navigate("../viewStudent");
-  };
 
   return (
     <Box m="20px">
@@ -19,16 +15,23 @@ const Attendance = () => {
         <Header title="ATTENDANCE" subtitle="Welcome to Attendance Page" />
       </Box>
 
-      <Box display="flex" flexDirection="row" justifyContent="center" margin={'5%'}>
-        <Button
-          type="submit"
-          color="secondary"
-          variant="contained"
-          onClick={handleButtonClick}
+      <Typography marginLeft="3%">Search for Student ID to proceed</Typography>
+
+      {/* SEARCH BAR and ICON BUTTON*/}
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Box
+          display="flex"
+          backgroundColor={colors.primary[400]}
+          borderRadius="3px"
+          margin="2%"
+          marginLeft="5%"
         >
-          Search Student
-        </Button>
-      </Box>
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+      </Grid>
     </Box>
   );
 };
